f3610a6917583145671490fc24eff936bc6c3160
Tanis Pérez Nartallo <tanisperez@gmail.com>
2022-07-06 20:49:32 +0200

    Add src folder

---
+++ src/pages/_app.js
@@ -0,0 +1,14 @@
+import '../../styles/globals.css'
+
+const LaBolsa = () => {
+    return (
+        <>
+            <h1>La Bolsa</h1>
+            <div>
+                Algo más...
+            </div>
+        </>
+    )
+}
+
+export default LaBolsa
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+import 'bootstrap/dist/css/bootstrap.css'
+
+const Home = () => {
+    return (
+        <h1>La Bolsa</h1>
+    )
+}
+
+export default Home
