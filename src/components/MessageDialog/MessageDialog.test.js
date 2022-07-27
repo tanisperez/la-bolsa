4f16a9a729d0621afbcbff4ff8de975a4b3d37a8
Tanis Pérez Nartallo <tanisperez@gmail.com>
2022-07-27 08:58:48 +0200

    Optimize imports for MessageDialog test

--- src/components/MessageDialog/MessageDialog.test.js
+++ src/components/MessageDialog/MessageDialog.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {render, fireEvent, waitFor, screen, queryByText} from '@testing-library/react';
+import {render, fireEvent, waitFor, screen} from '@testing-library/react';
 
 import MessageDialog from './MessageDialog';
 
