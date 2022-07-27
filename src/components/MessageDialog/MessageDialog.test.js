4932ddefc41be27a6b4199c6cfd61eb73fb98c0f
Tanis Pérez Nartallo <tanisperez@gmail.com>
2022-07-27 08:54:30 +0200

    Add accept test for MessageDialog

--- src/components/MessageDialog/MessageDialog.test.js
+++ src/components/MessageDialog/MessageDialog.test.js
@@ -32,6 +32,7 @@ describe('MessageDialog', () => {
 
         render(<MessageDialog message={message} onAccept={((data) => {})}/>);
 
+        expect(screen.getByRole('dialog')).toHaveClass('show');
         expect(screen.queryByText(message.title)).toBeInTheDocument();
         expect(screen.queryByText(message.buttons.cancelTitle)).toBeInTheDocument();
         expect(screen.queryByText(message.buttons.acceptTitle)).toBeInTheDocument();
@@ -61,6 +62,7 @@ describe('MessageDialog', () => {
             body: '¿Deseas eliminar Absolut?'
         };
         rerender(<MessageDialog message={message} onAccept={((data) => {})}/>);
+        expect(screen.getByRole('dialog')).toHaveClass('show');
         expect(screen.queryByText(message.title)).toBeInTheDocument();
         expect(screen.queryByText(message.buttons.cancelTitle)).toBeInTheDocument();
         expect(screen.queryByText(message.buttons.acceptTitle)).toBeInTheDocument();
@@ -105,5 +107,32 @@ describe('MessageDialog', () => {
         await waitFor(() => fireEvent.click(cancelButton));
 
         expect(screen.getByRole('dialog')).not.toHaveClass('show');
+    });
+
+    test('accept message dialog', async () => {
+        const message = {
+            show: true,
+            title: 'Eliminar una bebida',
+            buttons: {
+                cancelTitle: 'Cancelar',
+                acceptTitle: 'Aceptar'
+            },
+            body: '¿Deseas eliminar Absolut?',
+            data: {
+                result: true
+            }
+        };
+        let accepted = false;
+        const acceptEvent = (data) => accepted = data.result;
+
+        render(<MessageDialog message={message} onAccept={acceptEvent}/>);
+        expect(screen.getByRole('dialog')).toHaveClass('show');
+        expect(accepted).toBe(false);
+
+        const acceptButton = screen.getByText('Aceptar');
+        await waitFor(() => fireEvent.click(acceptButton));
+
+        expect(screen.getByRole('dialog')).not.toHaveClass('show');
+        expect(accepted).toBe(true);
     });
 });
