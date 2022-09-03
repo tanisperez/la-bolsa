a7c113b6a7df10f10ab55477b7f8ff60649c7951
Tanis Pérez Nartallo <tanisperez@gmail.com>
2022-09-03 20:49:38 +0200

    Use config from Next.js for path matching

--- src/middleware.js
+++ src/middleware.js
@@ -1,38 +1,28 @@
 import { NextResponse } from 'next/server';
 
-const REQUIRED_AUTH_PATHS = [
-    '/admin',
-    '/api/drinks'
-];
+export const config = {
+    matcher: [
+        '/admin',
+        '/api/drinks',
+        '/api/drinks/:path*'
+    ]
+};
 
 export function middleware(request) {
-    const url = request.nextUrl;
-
-    if (isAuthRequired(url.pathname)) {
-        const basicAuth = request.headers.get('authorization');
-        if (basicAuth) {
-            const [user, password] = getAuthentication(basicAuth);
-            if (user === 'admin' && password === 'admin') {
-                return NextResponse.next();
-            }
+    const basicAuth = request.headers.get('authorization');
+    if (basicAuth) {
+        const [user, password] = getAuthentication(basicAuth);
+        if (user === 'admin' && password === 'admin') {
+            return NextResponse.next();
         }
-        url.pathname = '/api/auth';
-        return NextResponse.rewrite(url);
     }
-    return NextResponse.next();
+    
+    const url = request.nextUrl;
+    url.pathname = '/api/auth';
+    return NextResponse.rewrite(url);
 }
 
 function getAuthentication(basicAuth) {
     const authValue = basicAuth.split(' ')[1];
     return atob(authValue).split(':');
-}
-
-function isAuthRequired(path) {
-    const lowerCasePath = path.toLowerCase();
-    for (let requiredAuth of REQUIRED_AUTH_PATHS) {
-        if (lowerCasePath.startsWith(requiredAuth)) {
-            return true;
-        }
-    }
-    return false;
 }
