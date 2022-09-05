7d56c5f1583a517fa357e0dd0c90059f8ec5b934
Tanis Pérez Nartallo <tanisperez@gmail.com>
2022-09-05 19:58:03 +0200

    Admin refactor with a tab system

--- src/pages/admin.js
+++ src/pages/admin.js
@@ -1,130 +1,29 @@
-import React, { useEffect, useState } from 'react';
-import { Container, Row, Col, Table, Button } from 'react-bootstrap';
-import { Pencil, XLg } from 'react-bootstrap-icons';
+import React, { useState } from 'react';
+import { Container, Row, Col, Table, Button, Navbar, Tabs, Tab } from 'react-bootstrap';
 
-import drinkClient from '@clients/DrinkClient';
-import AddDrink from '@components/Admin/AddDrink';
-import EditDrink from '@components/Admin/EditDrink';
-import MessageDialog from '@components/MessageDialog/MessageDialog';
+import DrinkList from '@components/Admin/DrinkList';
 
 const Admin = () => {
-    const [drinks, setDrinks] = useState([]);
-    const [addDrinkShow, setAddDrinkShow] = useState(false);
-    const [editDrinkShow, setEditDrinkShow] = useState(false);
-    const [editDrink, setEditDrink] = useState(undefined);
-    const [deleteDrinkMessage, setDeleteDrinkMessage] = useState({
-        show: false,
-        title: 'Eliminar una bebida',
-        buttons: {
-            cancelTitle: 'Cancelar',
-            acceptTitle: 'Eliminar'
-        }
-    });
-
-    const loadDrinks = () => {
-        drinkClient.getDrinks()
-            .then((result) => setDrinks(result))
-            .catch((error) => console.error(error));
-    };
-
-    const onHideAddDrinkModal = ((refresh) => {
-        const refreshDrinks = refresh || false;
-        setAddDrinkShow(false);
-        if (refreshDrinks) {
-            loadDrinks();
-        }
-    });
-
-    const onHideEditDrinkModal = ((refresh) => {
-        const refreshDrinks = refresh || false;
-        setEditDrinkShow(false);
-        if (refreshDrinks) {
-            loadDrinks();
-        }
-    });
-
-    const onDeleteDrink = ((data) => {
-        drinkClient.deleteDrink(data.drink_id)
-            .then(() => loadDrinks())
-            .catch((error) => console.error(error));
-    });
-
-    const openEditDrinkModal = (drinkId) => {
-        const drink = drinks.find((drink) => drink.drink_id == drinkId);
-        setEditDrink(drink);
-        setEditDrinkShow(true);
-    };
-
-    const openDeleteDrinkModal = (drinkId) => {
-        const drink = drinks.find((drink) => drink.drink_id == drinkId);
-        const { title, buttons } = deleteDrinkMessage;
-        const message = {
-            show: true,
-            title: title,
-            buttons: buttons,
-            body: `¿Deseas eliminar ${drink.name}?`,
-            data: drink
-        }
-        setDeleteDrinkMessage(message);
-    };
-
-    const isMarketEmpty = () => {
-        return drinks.length == 0;
-    }
-
-    useEffect(() => {
-        loadDrinks();
-    }, []);
+    const [key, setKey] = useState('drinks');
 
     return (
-        <Container className="admin-page">
-            <Row className="admin-navbar">
-                <Col xs className="px-0">
-                    <span>Página de administración</span>
-                </Col>
-                <Col xs="auto" className="px-0">
-                    <Button className="add-drink" onClick={() => setAddDrinkShow(true)}>Añadir bebida</Button>
-                </Col>
-            </Row>
-            <Row>
-                <Table striped bordered hover variant="dark">
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>Alias bolsa</th>
-                            <th>Nombre de la bebida</th>
-                            <th>Precio mínimo</th>
-                            <th>Precio máximo</th>
-                            <th className="edit-drink"></th>
-                            <th className="delete-drink"></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        { 
-                            isMarketEmpty() ? 
-                                <tr>
-                                    <td style={{textAlign: "center"}} colSpan={7}>No hay bebidas en el mercado</td>
-                                </tr>
-                            :
-                                drinks.map(drink => (
-                                    <tr key={drink.drink_id}>
-                                        <td>{drink.drink_id}</td>
-                                        <td>{drink.alias}</td>
-                                        <td>{drink.name}</td>
-                                        <td>{drink.min_price} €</td>
-                                        <td>{drink.max_price} €</td>
-                                        <td><Pencil size={20} onClick={() => openEditDrinkModal(drink.drink_id)}/></td>
-                                        <td><XLg size={20} onClick={() => openDeleteDrinkModal(drink.drink_id)}/></td>
-                                    </tr>
-                                )) 
-                        }
-                    </tbody>
-                </Table>
-            </Row>
-            <AddDrink show={addDrinkShow} onHide={onHideAddDrinkModal}/>
-            <EditDrink show={editDrinkShow} onHide={onHideEditDrinkModal} drink={editDrink}/>
-            <MessageDialog message={deleteDrinkMessage} onAccept={onDeleteDrink}/>
-        </Container >
+        <div className="admin-page">
+            <Navbar bg="primary" variant="dark" className="mb-3">
+                <Container>
+                    <Navbar.Brand className="fs-2 fw-bold">Página de administración</Navbar.Brand>
+                </Container>
+            </Navbar>
+            <Container>
+                <Tabs id="controlled-tab-example" activeKey={key} onSelect={(k) => setKey(k)} className="mb-3">
+                    <Tab eventKey="drinks" title="Bebidas">
+                        <DrinkList />
+                    </Tab>
+                    <Tab eventKey="config" title="Configuración">
+                        Configuración
+                    </Tab>
+                </Tabs>
+            </Container>
+        </div>
     );
 }
 
