15278935c7c1754f4e9e4a68420678f071c0c0ac
Tanis Pérez Nartallo <tanisperez@gmail.com>
2022-09-06 19:30:54 +0200

    Use 4 columns on xxl displays

--- src/components/market/Market.js
+++ src/components/market/Market.js
@@ -35,7 +35,7 @@ const Market = () => {
     }
     return (
         <div>
-            <Row className="mt-4 mb-4" xs={1} sm={1} md={2} lg={2} xl={2} xxl={3}>
+            <Row className="mt-4 mb-4" xs={1} sm={1} md={2} lg={2} xl={2} xxl={4}>
                 { 
                     drinks.map(drink => 
                         <Col key={drink.drink_id}>
