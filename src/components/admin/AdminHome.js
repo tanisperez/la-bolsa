9fd637fcff536b4e9816b5f0fe6ddc8f6f7f74e4
Tanis Pérez Nartallo <tanisperez@gmail.com>
2022-09-08 20:10:54 +0200

    Refactor crack button styles to a new css file

--- src/components/admin/AdminHome.js
+++ src/components/admin/AdminHome.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import styles from './AdminHome.module.css';
 
 const AdminHome = () => {
+    const [crackEnabled, setCrackEnabled] = useState(false);
+
+    const crackMode = () => {
+        setCrackEnabled(true);
+    };
+
     return (
         <div className="d-flex justify-content-center mt-4">
-            <div className="crack-button">
+            <button className={styles.crackButton} onClick={crackMode} disabled={crackEnabled}>
                 ¡Crack!
-            </div>
+            </button>
         </div>
     );
 };
