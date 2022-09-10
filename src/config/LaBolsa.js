3e3d81f3faeb298d188de6598286c59624e40ec6
Tanis Pérez Nartallo <tanisperez@gmail.com>
2022-09-10 14:13:57 +0200

    Add new constants to LaBolsa

--- src/config/LaBolsa.js
+++ src/config/LaBolsa.js
@@ -2,10 +2,16 @@
 export const PRICE_STEP = 0.5;
 
 // Backend market refresh time in minutes
-export const MARKET_REFRESH_PRICES_IN_MINUTES = 20;
+export const MARKET_REFRESH_PRICES_IN_MINUTES = 0.1;
 
 // Backend market refresh time in milliseconds
 export const MARKET_REFRESH_PRICES_IN_MILLIS = MARKET_REFRESH_PRICES_IN_MINUTES * 60 * 1000;
+
+// Backend market crack duration in minutes
+export const MARKET_CRACK_DURATION_IN_MINUTES = 1;
+
+// Backend market crack duration in milliseconds
+export const MARKET_CRACK_DURATION_IN_MILLIS = MARKET_CRACK_DURATION_IN_MINUTES * 60 * 1000;
 
 // Frontend market refresh time in seconds
 export const CLIENT_MARKET_REFRESH_TIME_IN_SECONDS = 10;
