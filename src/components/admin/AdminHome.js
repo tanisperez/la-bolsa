2132a942f255639dc4a00ca62bed26535e2bb6ab
Tanis Pérez Nartallo <tanisperez@gmail.com>
2022-09-11 12:41:01 +0200

    Enable and show crack mode in admin page

--- src/components/admin/AdminHome.js
+++ src/components/admin/AdminHome.js
@@ -1,24 +1,41 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
+import { MARKET_CRACK_DURATION_IN_MINUTES } from '@config/LaBolsa';
 import MarketClient from '@clients/MarketClient';
 
 import styles from './AdminHome.module.css';
 
 const AdminHome = () => {
-    const [crackEnabled, setCrackEnabled] = useState(false);
+    const [crackStatus, setCrackStatus] = useState({
+        crack_mode_start: undefined,
+        crack_mode_end: undefined,
+        enabled: false
+    });
+
+    useEffect(() => {
+        const marketClient = new MarketClient();
+
+        marketClient.getCrackModeStatus()
+            .then((status) => setCrackStatus(status))
+            .catch((error) => console.error(error));
+    }, []);
+
 
     const crackMode = async () => {
         const marketClient = new MarketClient();
         const status = await marketClient.enableCrackMode();
-        
-        setCrackEnabled(status.enabled);
-        
-        alert(JSON.stringify(status));
+        setCrackStatus(status);
     };
 
     return (
-        <div className="d-flex justify-content-center mt-4">
-            <button className={styles.crackButton} onClick={crackMode} disabled={crackEnabled}>
+        <div className="d-flex flex-column align-items-center mt-4">
+            {
+                crackStatus.enabled ?
+                    <span className="mb-4">El modo crack está habilitado, finalizará a las {crackStatus.crack_mode_end}</span>
+                :
+                    <span className="mb-4">La duración del modo crack será de {MARKET_CRACK_DURATION_IN_MINUTES} minutos</span>
+            }
+            <button className={styles.crackButton} onClick={crackMode} disabled={crackStatus.enabled}>
                 ¡Crack!
             </button>
         </div>
