1b79491f83986277767166347af2845db9ef8f72
Tanis Pérez Nartallo <tanisperez@gmail.com>
2022-09-21 19:25:08 +0200

    Fetch the market_crack_duration_in_minutes from the API

--- src/components/admin/AdminHome.js
+++ src/components/admin/AdminHome.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 
-import { MARKET_CRACK_DURATION_IN_MINUTES } from '@config/LaBolsa';
 import MarketClient from '@clients/MarketClient';
+import ConfigClient from '@clients/ConfigClient';
 
 import styles from './AdminHome.module.css';
 
@@ -11,12 +11,20 @@ const AdminHome = () => {
         crack_mode_end: undefined,
         enabled: false
     });
+    const [config, setConfig] = useState({
+        market_refresh_time_in_minutes: undefined,
+        market_crack_duration_in_minutes: undefined
+    });
 
     useEffect(() => {
         const marketClient = new MarketClient();
-
         marketClient.getCrackModeStatus()
             .then((status) => setCrackStatus(status))
+            .catch((error) => console.error(error));
+
+        const configClient = new ConfigClient();
+        configClient.getConfig()
+            .then((config) => setConfig(config))
             .catch((error) => console.error(error));
     }, []);
 
@@ -32,8 +40,8 @@ const AdminHome = () => {
             {
                 crackStatus.enabled ?
                     <span className="mb-4">El modo crack está habilitado, finalizará a las {crackStatus.crack_mode_end}</span>
-                :
-                    <span className="mb-4">La duración del modo crack será de {MARKET_CRACK_DURATION_IN_MINUTES} minutos</span>
+                    :
+                    <span className="mb-4">La duración del modo crack será de {config.market_crack_duration_in_minutes} minutos</span>
             }
             <button className={styles.crackButton} onClick={crackMode} disabled={crackStatus.enabled}>
                 ¡Crack!
