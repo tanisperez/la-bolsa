caa3e2ebd7a3d8d9000d0873bf0be32591e3235a
Tanis Pérez Nartallo <tanisperez@gmail.com>
2022-09-21 19:39:12 +0200

    Remove unused constants in LaBolsa.js

--- src/config/LaBolsa.js
+++ src/config/LaBolsa.js
@@ -1,12 +1,6 @@
 // Prices increments and decrements
 export const PRICE_STEP = 0.5;
 
-// Backend market crack duration in minutes
-export const MARKET_CRACK_DURATION_IN_MINUTES = 5;
-
-// Backend market crack duration in milliseconds
-export const MARKET_CRACK_DURATION_IN_MILLIS = MARKET_CRACK_DURATION_IN_MINUTES * 60 * 1000;
-
 // Frontend market refresh time in seconds
 export const CLIENT_MARKET_REFRESH_TIME_IN_SECONDS = 10;
 
