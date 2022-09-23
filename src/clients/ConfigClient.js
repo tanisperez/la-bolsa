b8c2a5c83209af3ba6ca0ad9154ef6407215f038
Tanis Pérez Nartallo <tanisperez@gmail.com>
2022-09-23 16:54:18 +0200

    Add method in ConfigClient to modify config params

--- src/clients/ConfigClient.js
+++ src/clients/ConfigClient.js
@@ -3,6 +3,18 @@ class MarketClient {
         const response = await fetch('/api/config');
         return await response.json();
     }
+
+    async editConfig(config) {
+        const response = await fetch(`/api/admin/config`, {
+            method: 'PUT',
+            body: JSON.stringify(config),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        const modifiedConfig = await response.json();
+        return modifiedConfig;
+    }
 }
 
 export default MarketClient;
