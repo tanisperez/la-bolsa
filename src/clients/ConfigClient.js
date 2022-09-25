8c5b1a74b0932efbbffdbde143f91ab6cab7f319
Tanis Pérez Nartallo <tanisperez@gmail.com>
2022-09-25 21:33:03 +0200

    Catch error con getConfig

--- src/clients/ConfigClient.js
+++ src/clients/ConfigClient.js
@@ -1,6 +1,10 @@
 class MarketClient {
     async getConfig() {
         const response = await fetch('/api/config');
+        if (!response.ok) {
+            const text = await response.text();
+            throw new Error(`Se produjo un error con el código ${response.status} y el mensaje ${text}`);
+        }
         return await response.json();
     }
 
