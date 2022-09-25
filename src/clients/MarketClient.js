908d42bd261ff4d6792d8240b790de99abc4cb09
Tanis Pérez Nartallo <tanisperez@gmail.com>
2022-09-25 21:59:02 +0200

    Catch errors in the MarketClient

--- src/clients/MarketClient.js
+++ src/clients/MarketClient.js
@@ -1,11 +1,19 @@
 class MarketClient {
     async getMarket() {
         const response = await fetch('/api/market');
+        if (!response.ok) {
+            const text = await response.text();
+            throw new Error(`Se produjo un error con el código ${response.status} y el mensaje ${text}`);
+        }
         return await response.json();
     }
 
     async getCrackModeStatus() {
         const response = await fetch('/api/admin/crack/status');
+        if (!response.ok) {
+            const text = await response.text();
+            throw new Error(`Se produjo un error con el código ${response.status} y el mensaje ${text}`);
+        }
         return await response.json();
     }
 
@@ -13,6 +21,10 @@ class MarketClient {
         const response = await fetch('/api/admin/crack/enable', {
             method: 'PUT'
         });
+        if (!response.ok) {
+            const text = await response.text();
+            throw new Error(`Se produjo un error con el código ${response.status} y el mensaje ${text}`);
+        }
         return await response.json();
     }
 }
